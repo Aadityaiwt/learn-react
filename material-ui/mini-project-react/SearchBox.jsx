de691e3287b4a450b79e3eb0cbbc0690489c4c25
Adityaiwt <aadityakumar4984@gmail.com>
2026-02-20 12:48:17 +0530

    Change API URL

--- material-ui/mini-project-react/SearchBox.jsx
+++ material-ui/mini-project-react/SearchBox.jsx
@@ -6,24 +6,24 @@ import { useState } from 'react';
 export default function SearchBox() {
     let [city, setCity] = useState("");
 
-    const API_URL = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
     const API_KEY = "b78554c50ebc7ed6b4a1d9a03d416ec5";
 
     let getWeatherInfo = async () => {
-        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`);
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         let jsonResponce = await response.json();
         console.log(jsonResponce);
 
-        // let result= {
-        //     temp: jsonResponce.main.temp,
-        //     tempMin: jsonResponce.main.temp_min,
-        //     tempMax: jsonResponce.main.temp_max,
-        //     tempMax: jsonResponce.main.temp_max,
-        //     humidity: jsonResponce.main.humidity,
-        //     feelsLike: jsonResponce.main.feels_like,
-        //     weather: jsonResponce.weather[0].description
-        // }
-        // console.log(result);
+        let result= {
+            temp: jsonResponce.main.temp,
+            tempMin: jsonResponce.main.temp_min,
+            tempMax: jsonResponce.main.temp_max,
+            tempMax: jsonResponce.main.temp_max,
+            humidity: jsonResponce.main.humidity,
+            feelsLike: jsonResponce.main.feels_like,
+            weather: jsonResponce.weather[0].description
+        }
+        console.log(result);
         
     };
 
